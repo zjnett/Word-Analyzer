e4c27297abb61586e139b50cf3fd49f2fa2cc613
Zach Nett <zjnett@Zachs-iMac-Pro.local>
2020-05-28 13:46:02 -0700

    Added thread functions and threads for (most) calculation steps

--- main.cpp
+++ main.cpp
@@ -13,19 +13,19 @@
         (right now code does double the work that it needs to)
 */
 
-void longWord(Analyzer &a) {
+void longWord(Analyzer a) {
     std::cout << "Longest word is " << a.findLongestWord() << std::endl;
 }
 
-void shortWord(Analyzer &a) {
+void shortWord(Analyzer a) {
     std::cout << "Shortest word is " << a.findShortestWord() << std::endl;
 }
 
-void averageWordLength(Analyzer &a) {
+void averageWordLength(Analyzer a) {
     std::cout << "Average word length is " << a.findAverageWordLength() << std::endl;
 }
 
-void mostCommonWords(Analyzer &a) {
+void mostCommonWords(Analyzer a) {
     std::vector<std::string> most_common_words = a.findMostCommonWords();
     a.printCommonWords(most_common_words);
 }
@@ -43,22 +43,30 @@ int main(int argc, char **argv) {
     std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 
     if (a.loadText(filename)) {
+        //          SINGLE-THREADED SECTION
         /*std::string longest_word = a.findLongestWord();
         std::string shortest_word = a.findShortestWord();
         double average_length = a.findAverageWordLength();
         std::vector<std::string> most_common_words = a.findMostCommonWords();*/
 
-        std::cout << "Text loaded successfully!" << std::endl;
+        //std::cout << "Text loaded successfully!" << std::endl;
         //a.printFrequency();
-        longWord(a);
-        shortWord(a);
-        averageWordLength(a);
-        mostCommonWords(a);
-        a.printWordCount();
+
+        //          MULTI-THREADED SECTION
+        std::thread long_word_thread(longWord, a);
+        std::thread short_word_thread(shortWord, a);
+        std::thread average_word_length_thread(averageWordLength, a);
+        std::thread most_common_words_thread(mostCommonWords, a);
+
+        long_word_thread.join();
+        short_word_thread.join();
+        average_word_length_thread.join();
+        most_common_words_thread.join();
 
         std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> time_took = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
 
+        a.printWordCount();
         std::cout << "Program took " << time_took.count() << " seconds to calculate." << std::endl;
 
         if (a.printDataToFile()) {
